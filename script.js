22c8f8e2c1de2cdfcf6f2d75467e0a874351a576
Alice Goward <alice.goward@spgs.org>
2023-02-17 17:56:56 +0000

    started adding graphics

--- script.js
+++ script.js
@@ -1,11 +1,44 @@
-var timeLeft = 3540
-var guess;
+var timeLeft = 0
+var qon = 0
+var correct = 0
+var wrong = 0
+var nq = 24
+var started = false
+var enter = false
+var w = 0
+var pinksq;
+var greysq;
 var qs = [
+    ['salt 1','F'],
+    ['salt 2','T'],
+    ['salt 3','F'],
+    ['salt 4','T'],
+    ['salt 1','F'],
+    ['salt 2','T'],
+    ['salt 3','F'],
+    ['salt 4','T'],
+    ['salt 1','F'],
+    ['salt 2','T'],
+    ['salt 3','F'],
+    ['salt 4','T'],
+    ['salt 1','F'],
+    ['salt 2','T'],
+    ['salt 3','F'],
+    ['salt 4','T'],
+    ['salt 1','F'],
+    ['salt 2','T'],
+    ['salt 3','F'],
+    ['salt 4','T'],
     ['salt 1','F'],
     ['salt 2','T'],
     ['salt 3','F'],
     ['salt 4','T']
 ]
+
+function preload() {
+    pinksq = loadImage('assets/pink.png')
+    greysq = loadImage('assets/grey.png')
+}
 
 function setup() {
     createCanvas(windowWidth,windowHeight);
@@ -14,23 +47,80 @@ function setup() {
   
 function gameOver() {
     text('Game Over',100,100)
+    if (enter == true) {
+        started = false
+        enter=false
+    }
+}
+function beatGame() {
+    text('You have completed all the questions',200,200)
+    if (enter == true) {
+        started = false
+        enter=false
+    }
+}
+
+function welcome() {
+    text('Press enter to start',100,100)
+    if (enter == true) {
+        started = true
+        enter=false
+        qon=0
+        timeLeft = 3540
+    }
+}
+
+function boxes(correct, wrong) {
+    text('Correct',50,windowHeight-100)
+    var npc = Math.floor(((windowWidth/2)-100)/40)
+    for (i=0;i<correct;i++) {
+        ex=i%npc*40+50
+        ey = Math.floor(i/npc)*40+50
+        image(pinksq,ex,ey,32,32)
+    }
 }
 
 function draw() {
     background(220);
-    if (timeLeft>0) {
-        timeLeft--
-        time=Math.ceil(timeLeft/60)
-        textSize(100)
-        text(time,100,100)
-        text(qs[0][0],200,200)
+    textSize(50)
+    boxes(correct,wrong)
+    if (started == false) {
+        welcome()
+    } else if (timeLeft>0) {
+        if (qon>=nq) {
+            beatGame()
+        } else {
+            timeLeft--
+            time=Math.ceil(timeLeft/60)
+            text(time,100,100)
+            text(qs[qon][0],200,200)
+        }
     } else {
         gameOver()
     }
 }
 
 function keyPressed() {
-    if (keyCode === 84) {
-        guess = 'T'
+    if (qon !=nq && timeLeft !=0) {
+        if (keyCode === 84) {
+            if (qs[qon][1] == 'T') {
+                correct++
+            } else{
+                wrong++
+                w++
+            }
+            qon++
+        }
+        if (keyCode === 70) {
+            if (qs[qon][1] == 'F') {
+                correct++
+            } else{
+                w++
+            }
+            qon++
+        }
+    }
+    if (keyCode === 13) {
+        enter = true
     }
 }
